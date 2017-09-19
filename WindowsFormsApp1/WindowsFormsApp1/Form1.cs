61574e39aeeb349118bc5ca1cd06b0d715ab7fd3
Seth Tilley <370990@edu.cwa.ac.uk>
2017-09-19 10:45:00 +0100

    Added the seth windows form

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -30,7 +30,8 @@ namespace WindowsFormsApp1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            Seth s = new Seth();
+            s.ShowDialog();
         }
 
         private void button3_Click(object sender, EventArgs e)
