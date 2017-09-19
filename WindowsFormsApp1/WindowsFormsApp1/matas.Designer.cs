2183bf2dc0c8c9ba7b804223a000dd5a87b8a7b0
ma tas <matas9745@gmail.com>
2017-09-19 10:52:37 +0100

    matas2

--- WindowsFormsApp1/WindowsFormsApp1/matas.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/matas.Designer.cs
@@ -28,11 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.MMM = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // MMM
+            // 
+            this.MMM.AutoSize = true;
+            this.MMM.Location = new System.Drawing.Point(90, 60);
+            this.MMM.Name = "MMM";
+            this.MMM.Size = new System.Drawing.Size(34, 13);
+            this.MMM.TabIndex = 0;
+            this.MMM.Text = "MMM";
+            this.MMM.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // matas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.MMM);
+            this.Name = "matas";
             this.Text = "matas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label MMM;
     }
 }
