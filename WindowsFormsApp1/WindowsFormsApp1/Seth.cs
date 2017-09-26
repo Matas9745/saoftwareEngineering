42121f69080fa8b38be6de5c703da5813ce2c8a5
Seth Tilley <370990@edu.cwa.ac.uk>
2017-09-26 09:06:57 +0100

    Added form feilds and preparing to add class

--- WindowsFormsApp1/WindowsFormsApp1/Seth.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Seth.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void btn_Spoilers_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
